a6fb8c29ec7709f5337a753f848453b1a911bd07
chehin <chehinjuan@gmail.com>
2022-02-23 08:50:14 -0300

    for + site en .env

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 async function main() {
-    console.log("pasa")
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false, ignoreHTTPSErrors: true });
     const page = await browser.newPage();
 
     await page.setViewport({ width: 1200, height: 720 });
-    await page.goto('https://www.badoo.com/signin', { waitUntil: 'networkidle0' }); // wait until page load
+    await page.goto(process.env.site, { waitUntil: 'networkidle0' }); // wait until page load
 
     await page.type('[type="text"]', process.env.user);
     await page.type('[type="password"]', process.env.password);
@@ -18,9 +17,21 @@ async function main() {
         // espera a cargar sitio
     await page.waitForTimeout(11000);
 
-    let f = await page.$(".encounters-actions__item.encounters-actions__item--yes");
-    await f.click();
+    let button = await page.$(".encounters-actions__item.encounters-actions__item--yes");
+    page.waitForTimeout(25000)
 
+    var i
+    for (i = 0; i <= 20; i++) {
+        console.log("pasa ", i)
+
+        await page.waitForTimeout(10000)
+
+        console.log("pasa 1", i)
+
+        await button.click() // Clicking the link will indirectly cause a navigation
+
+        console.log("pasa 2", i)
+    }
 }
 
 main();
